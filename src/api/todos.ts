dc69d04e33647f027fecdeaddbd3155d564fc184
anastasiiadns <nastyadenisenko17@gmail.com>
2026-05-22 11:41:49 +0300

    task solution

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,10 +1,11 @@
 import { Todo } from '../types/Todo';
 import { client } from '../utils/fetchClient';
 
-export const USER_ID = 0;
+export const USER_ID = 4202;
 
 export const getTodos = () => {
   return client.get<Todo[]>(`/todos?userId=${USER_ID}`);
 };
 
 // Add more methods here
+// Додати сюди більше методів
